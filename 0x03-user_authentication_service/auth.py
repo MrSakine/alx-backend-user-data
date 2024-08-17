1516bc5d413db48976cf8ac16f21346710923989
MrSakine <msakine20@gmail.com>
2024-08-17 19:01:29 +0000

    removed self keyword

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -21,7 +21,7 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """Generates a new UUID and returns it as a string"""
     return str(uuid.uuid4())
 
