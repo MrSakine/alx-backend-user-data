76c7a7987d9688cc79c9b87fcf18b3110bb6fb1a
MrSakine <msakine20@gmail.com>
2024-08-17 18:40:31 +0000

    removed validation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -19,12 +19,11 @@ def users():
     email = request.form.get("email")
     password = request.form.get("password")
 
-    if not email or not password:
-        return jsonify({"message": "email and password are required"}), 400
-
     try:
         user = AUTH.register_user(email=email, password=password)
-        return jsonify({"email": user.email, "message": "user created"}), 201
+        return jsonify(
+            {"email": user.email, "message": "user created"}
+        ), 201
     except ValueError as e:
         return jsonify({"message": str(e)}), 400
 
