1f45b2079d92f38b2200d1f5b8c1e52a73b5f4ba
MrSakine <msakine20@gmail.com>
2024-08-17 18:44:43 +0000

    ValueError message update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -24,8 +24,10 @@ def users():
         return jsonify(
             {"email": user.email, "message": "user created"}
         ), 201
-    except ValueError as e:
-        return jsonify({"message": str(e)}), 400
+    except ValueError:
+        return jsonify(
+            {"message": "email already registered"}
+        ), 400
 
 
 if __name__ == "__main__":
